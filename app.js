fb84bd475bfd12290c1200eaa0062dcde82f6853
Jessica <jhanccove@unsa.edu.pe>
2025-02-10 02:09:05 -0500

    Update app.js, functions 'agregarAmigo()' y 'actualizarListaAmigos()'

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos =[];
+
+function agregarAmigo(){
+    //let nuevoAmigo = document.getElementById("nuevoAmigo").value;
+    let nuevoAmigo = document.getElementById("amigo").value;
+    if(nuevoAmigo == ""){
+        alert("Por favor, ingresar un nombre");
+    } else {
+        amigos.push(nuevoAmigo);
+        document.querySelector("#amigo").value = "";
+    }
+    console.log(amigos);
+    actualizarListaAmigos();
+}
+
+function actualizarListaAmigos(){
+    let listaActualizadaAmigos = document.getElementById("listaAmigos");
+    listaActualizadaAmigos.innerHTML = "";
+    for(let i = 0; i < amigos.length; i++){
+        listaActualizadaAmigos.innerHTML += "<li>" + amigos[i] + "</li>";
+    }
+}
